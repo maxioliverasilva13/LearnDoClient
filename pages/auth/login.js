e236f4f30fbc4d9bff0e78818bca65c9d11816e0
Manuel Diu <mandiu1@hotmail.com>
2023-04-19 01:09:23 -0300

    prepare login form
    node v16.14.0

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,55 +1,82 @@
 import React from "react";
 import Link from "next/link";
+import Alert from "components/Popups/Alert";
 
 // layout for page
 
 import Auth from "layouts/Auth.js";
 
 export default function Login() {
+  const [showAlert, setShowAlert] = React.useState(true);
+
+  
+  const handleSubmit = async (e, data) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const remember = document.getElementById('customCheckLogin').value;
+    console.log(remember);
+    if(!email){
+      setShowAlert(true);
+    }
+    // console.log(data)
+
+    // try {
+    // const res = await fetch('http://localhost:8080/api/login',
+    // { 
+    //   method: 'POST',
+    //   body: JSON.stringify({
+    //     query: data,
+    //   }),
+    // }
+    // ).then((res) => {
+    //   if(!res.ok){
+    //     setShowAlert(true);
+    //   }
+    // });
+    // } catch (err) {
+    //   console.log('Error en trycatch login.js: ' + err);      
+    // }
+
+  }
+
   return (
     <>
       <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
+        <div className="flex flex-col content-center items-center justify-center h-full">
+          {showAlert ? (
+            <div className="text-white w-1/2 px-6 py-4 border-0 rounded relative mb-4 bg-red-500">
+              <span className="text-xl inline-block mr-5 align-middle">
+                <i className="fas fa-bell" />
+              </span>
+              <span className="inline-block align-middle mr-8">
+                <b className="capitalize">Atención!</b> Email o Contraseña incorrectos.
+              </span>
+              <button className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
+                onClick={() => setShowAlert(false)}
+              >
+                <span>×</span>
+              </button>
+            </div>
+          ) : null}
+          <div className="w-full lg:w-4/12">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign in with
-                  </h6>
-                </div>
-                <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img alt="..." className="w-5 mr-1" src="/img/github.svg" />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                    type="button"
-                  >
-                    <img alt="..." className="w-5 mr-1" src="/img/google.svg" />
-                    Google
-                  </button>
-                </div>
-                <hr className="mt-6 border-b-1 border-blueGray-300" />
-              </div>
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+              <div className="flex-auto px-4 lg:px-10 py-10">
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
+                  <small>Inicia sesión utilizando tus credenciales</small>
                 </div>
-                <form>
+                <form action="#" method="POST" onSubmit={handleSubmit}>
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
+                      htmlFor="email"
                     >
                       Email
                     </label>
                     <input
                       type="email"
+                      id="email"
+                      name="email"
+                      autoComplete="email"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                       placeholder="Email"
                     />
@@ -58,14 +85,17 @@ export default function Login() {
                   <div className="relative w-full mb-3">
                     <label
                       className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
+                      htmlFor="password"
                     >
-                      Password
+                      Contraseña
                     </label>
                     <input
                       type="password"
+                      id="password"
+                      name="password"
+                      autoComplete="current-password"
                       className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
+                      placeholder="Contraseña"
                     />
                   </div>
                   <div>
@@ -73,10 +103,12 @@ export default function Login() {
                       <input
                         id="customCheckLogin"
                         type="checkbox"
+                        name="remember"
+                        value={"yes"}
                         className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
                       />
                       <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                        Remember me
+                        Recordar mis credenciales
                       </span>
                     </label>
                   </div>
@@ -84,7 +116,7 @@ export default function Login() {
                   <div className="text-center mt-6">
                     <button
                       className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
+                      type="submit"
                     >
                       Sign In
                     </button>
